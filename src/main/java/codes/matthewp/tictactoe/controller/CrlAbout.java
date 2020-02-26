554392983d8a68b522312a910abe757df5b329d7
xXXIMMMATTHEWXXx <mattparksjr@gmail.com>
2020-02-26 11:28:33 -0500

    Made the about screen fully translatable

--- src/main/java/codes/matthewp/tictactoe/controller/CrlAbout.java
+++ src/main/java/codes/matthewp/tictactoe/controller/CrlAbout.java
@@ -1,11 +1,34 @@
 package codes.matthewp.tictactoe.controller;
 
+import codes.matthewp.tictactoe.TicTacToe;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
-public class CrlAbout {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CrlAbout implements Initializable {
+
+    @FXML
+    private Label aboutTitle;
+
+    @FXML
+    private Text aboutText;
+
+    @FXML
+    private Button aboutExit;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        aboutTitle.setText(TicTacToe.lang.getString("aboutTitle"));
+        aboutText.setText(TicTacToe.lang.getString("aboutText"));
+        aboutExit.setText(TicTacToe.lang.getString("btnExit"));
+    }
 
     @FXML
     public void doClose(MouseEvent event) {
